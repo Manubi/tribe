a9e782958702998141d0f87dd84323fe233a8150
Daniel Arnauer <da@viewar.com>
2023-03-25 15:19:01 +0100

    Add AccountCard, LivingCommunityAccount

---
+++ src/components/AccountCard.tsx
@@ -0,0 +1,43 @@
+export function AccountCard({ card }: any) {
+  return (
+    <div className="max-w-6xl px-4 mx-auto sm:px-6 lg:px-8 m-10">
+      <div
+        key={card.name}
+        className="overflow-hidden bg-white rounded-lg shadow"
+      >
+        <div className="p-5">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <card.icon className="w-6 h-6 text-gray-400" aria-hidden="true" />
+            </div>
+            <div className="flex-1 w-0 ml-5">
+              <dl>
+                <dt className="text-sm font-medium text-black truncate">
+                  {card.name}
+                </dt>
+                <dt className="text-sm font-medium text-gray-500 truncate">
+                  {card.description}
+                </dt>
+                <dd>
+                  <div className="text-lg font-medium text-gray-900">
+                    {card.amount}
+                  </div>
+                </dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+        <div className="px-5 py-3 bg-gray-50">
+          <div className="text-sm">
+            <a
+              href={card.href}
+              className="font-medium text-cyan-700 hover:text-cyan-900"
+            >
+              View
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
