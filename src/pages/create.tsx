1133c0d0dbcc50848c15617c48a26096d350e5a6
Manuel Bichler <mbichler@cemm.at>
2023-03-25 17:04:37 +0100

    add create

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,3 +1,4 @@
+import InviteMembers from "@/components/Combobox";
 import Tabbar from "@/components/Tabbar";
 import Head from "next/head";
 
@@ -29,7 +30,7 @@ export default function Create() {
                   Account name
                 </label>
                 <div className="mt-2">
-                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-700 sm:max-w-md">
                     <span className="flex items-center pl-3 text-gray-500 select-none sm:text-sm"></span>
                     <input
                       type="text"
@@ -37,11 +38,39 @@ export default function Create() {
                       id="username"
                       autoComplete="username"
                       className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                      placeholder="janesmith"
+                      placeholder="Our shared account"
                     />
                   </div>
                 </div>
               </div>
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Money members need to add
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-cyan-700 sm:max-w-md">
+                    <span className="flex items-center pl-3 text-gray-500 select-none sm:text-sm"></span>
+                    <input
+                      type="number"
+                      name="username"
+                      id="username"
+                      autoComplete="username"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="200,- EUR"
+                    />
+                  </div>
+                </div>
+              </div>
+              <InviteMembers />
+              <button
+                type="button"
+                className="px-3 py-2 text-sm font-semibold text-center text-white rounded-md shadow-sm px-auto bg-cyan-600 hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+              >
+                Create shared account
+              </button>
             </div>
           </div>
         </div>
