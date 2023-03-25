877c45b972714a5f9451b77ebd7c0d8ee5cd66bb
Manuel Bichler <mbichler@cemm.at>
2023-03-25 18:26:29 +0100

    some

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -1,10 +1,10 @@
+import LivingCommunityAccount from "@/components/LivingCommunityAccount";
 import Tabbar from "@/components/Tabbar";
 
 export default function Wallet() {
   return (
     <>
-      <div className="text-gray-900 h-100">Wallet</div>
-      <p>asdf</p>
+      <LivingCommunityAccount />
       <Tabbar />
     </>
   );
